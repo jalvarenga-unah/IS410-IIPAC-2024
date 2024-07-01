8772ab4ac2298733e434474695f4ffd5c954bfae
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-07-01 17:47:18 -0600

    Manejo de controles y hashmap

---
+++ ui/Controles/src/controles/Controles.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controles;
+
+/**
+ *
+ * @author juanalvarenga
+ */
+public class Controles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginPage login = new LoginPage();
+        
+        login.setVisible(true);
+    }
+    
+}
